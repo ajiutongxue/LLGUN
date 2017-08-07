af042d226c930ea37cb537d125a33cb885ea8374
Yang <85103350@qq.com>
2017-08-07 14:35:16 +0800

    add 框架布局样式

---
+++ src/app/all-com/all-com.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'all-component',
+    templateUrl: './all-com.component.html'
+})
+
+export class AllComponentComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ModalComponent } from './modal/modal.component';
+// import { ModalComponent } from './modal/modal.component';
 import { EditorStringInputComponent } from './editor-string-input/editor-string-input.component';
 import { StringInputComponent } from './string-input/string-input.component';
 
 import { jqxWindowComponent } from '../assets/jqwidgets-ts/angular_jqxwindow';
 import { TextComponent } from './text/text.component';
-import { FatherComponent } from './father/father.component';
-import { ChildComponent } from './child/child.component';
+// import { FatherComponent } from './father/father.component';
+// import { ChildComponent } from './child/child.component';
 import { SelectInputComponent } from './select-input/select-input.component';
 //import { HighlightDirective } from './highlight.directive';
+
+import { LayoutComponent } from './layout/layout.component';
+import { AllComponentComponent } from './all-com/all-com.component';
+
+const appRoutes: Routes = [
+
+  {
+    path: 'layout',
+    component: LayoutComponent
+  },
+  {
+    path: 'all',
+    component: AllComponentComponent
+  },
+  {
+    path: '',
+    redirectTo: '/all',
+    pathMatch: 'full' // 这个full是必须的
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModalComponent,
+    // ModalComponent,
     EditorStringInputComponent,
     StringInputComponent,
     jqxWindowComponent,
     TextComponent,
-    FatherComponent,
-    ChildComponent,
+    // FatherComponent,
+    // ChildComponent,
     SelectInputComponent,
+    LayoutComponent,
+    AllComponentComponent
     //HighlightDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
