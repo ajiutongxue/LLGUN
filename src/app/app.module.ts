3b0482fa5b0f3a745570e3fd5d5ad541349ee946
Yang <85103350@qq.com>
2017-08-15 20:09:54 +0800

    add 日期组件、缩略图

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 // import { ModalComponent } from './modal/modal.component';
@@ -8,11 +9,16 @@ import { EditorStringInputComponent } from './editor-string-input/editor-string-
 import { StringInputComponent } from './string-input/string-input.component';
 
 import { jqxWindowComponent } from '../assets/jqwidgets-ts/angular_jqxwindow';
+// import { MyDatePickerModule } from 'mydatepicker';
+import { MyDatePickerModule } from '../assets/mydatepicker-master/src/my-date-picker/my-date-picker.module';
+
 import { TextComponent } from './text/text.component';
 // import { FatherComponent } from './father/father.component';
 // import { ChildComponent } from './child/child.component';
 import { SelectInputComponent } from './select-input/select-input.component';
 //import { HighlightDirective } from './highlight.directive';
+
+import { ThumbnailComponent } from './thumbnail/thumbnail.component';
 
 import { LayoutComponent } from './layout/layout.component';
 import { AllComponentComponent } from './all-com/all-com.component';
@@ -45,12 +51,15 @@ const appRoutes: Routes = [
     // FatherComponent,
     // ChildComponent,
     SelectInputComponent,
+    ThumbnailComponent,
     LayoutComponent,
-    AllComponentComponent
+    AllComponentComponent,
+    
     //HighlightDirective
   ],
   imports: [
     BrowserModule,
+    MyDatePickerModule,ReactiveFormsModule, FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
---
+++ src/app/thumbnail/thumbnail.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thumbnail',
+    templateUrl: './thumbnail.component.html',
+    styleUrls: ['./thumbnail.component.less']
+})
+
+export class ThumbnailComponent {
+    curRange: number = 1.5;
+    dataSource = [
+        {
+            show: 1,
+            name: '名称',
+            value: '项目一自称一'
+        },
+        {
+            show: 1,
+            name: '创建人',
+            value: '刘大宝'
+        },
+        {
+            show: 1,
+            name: '创建时间',
+            value: '2016-12-12 '
+        },
+    ];
+
+    showRange(e) {
+        this.curRange = e.target.value;
+    }
+}
