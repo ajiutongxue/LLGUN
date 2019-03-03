59816a6bf3544cd36f95f7f44fba14eb0fa7c76f
ajiutongxue <ajiutongxue@gmail.com>
2019-03-03 21:06:55 +0800

    alert box 组件

---
+++ src/app/my-new-task/alert-dialog/alert-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-dialog',
+  templateUrl: './alert-dialog.component.html',
+  styleUrls: ['./alert-dialog.component.less']
+})
+export class AlertDialogComponent implements OnInit {
+
+    isShowAlert = true;
+    mask = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
